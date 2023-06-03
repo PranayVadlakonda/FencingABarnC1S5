66f5c0907ba92e2e61a9de1a4ef5c344c60248ad
pranay <pranayanand61@gmail.com>
2023-06-03 14:24:31 +0530

    create a variable of class name for calling the method

--- src/main/java/com/niit/jdp/AreaCalculator.java
+++ src/main/java/com/niit/jdp/AreaCalculator.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class AreaCalculator {
     public static void main(String[] args) {
         // initialize a scanner
-        Scanner sc = new Scanner(system.in);
+        Scanner sc = new Scanner(System.in);
         // take side as input from user
         double side = sc.nextDouble();
         // take radius as input from user
@@ -13,6 +13,9 @@ public class AreaCalculator {
         // take length and width as input from user
         double length = sc.nextDouble();
         double width = sc.nextDouble();
+        // create a variable of class name for calling the method
+        AreaCalculator area = new AreaCalculator();
+
 
 
 
