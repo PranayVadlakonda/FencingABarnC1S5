fcd655e0a6b7ce7a0687ff91ac165376bed855a5
pranay <pranayanand61@gmail.com>
2023-06-03 14:14:19 +0530

    initialize a scanner

---
+++ src/main/java/com/niit/jdp/AreaCalculator.java
@@ -0,0 +1,11 @@
+package com.niit.jdp;
+
+import java.util.Scanner;
+
+public class AreaCalculator {
+    public static void main(String[] args) {
+        // initialize a scanner
+        Scanner sc = new Scanner(system.in);
+
+    }
+}
