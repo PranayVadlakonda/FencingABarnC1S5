d2c0ca34c67b14730cd74270e1a663eb79f2ab6b
pranay <pranayanand61@gmail.com>
2023-06-03 14:17:34 +0530

    take side as input from user

--- src/main/java/com/niit/jdp/AreaCalculator.java
+++ src/main/java/com/niit/jdp/AreaCalculator.java
@@ -6,6 +6,9 @@ public class AreaCalculator {
     public static void main(String[] args) {
         // initialize a scanner
         Scanner sc = new Scanner(system.in);
+        // take side as input from user
+        double side = sc.nextDouble();
+
 
     }
 }
