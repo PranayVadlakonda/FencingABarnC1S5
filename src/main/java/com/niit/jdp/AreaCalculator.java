126e48b5ba8942b615b4ba170e446c08771ae402
pranay <pranayanand61@gmail.com>
2023-06-03 14:41:28 +0530

    create a method for area of square

--- src/main/java/com/niit/jdp/AreaCalculator.java
+++ src/main/java/com/niit/jdp/AreaCalculator.java
@@ -18,14 +18,14 @@ public class AreaCalculator {
         // call the area of square method and calculate the area of square using that
         System.out.println(AreaCalculator.areaOfSquare(side));
         // call the area of circle method and calculate the area of circle using that
-        System.out.println(AreaCalculator.areaOfSquare(radius));
+        System.out.println(AreaCalculator.areaOfCircle(radius));
         // call the area of rectangle method and calculate the area of rectangle using that
         System.out.println(AreaCalculator.areaOfRectangle(length,width));
 
-
-
-
-
-
+    }
+    // create a method for area of square
+    public static double areaOfSquare(double side){
+        double areaOfSquare= (side*side);
+        return areaOfSquare;
     }
 }
